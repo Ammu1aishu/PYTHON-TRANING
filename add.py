3020ff0860ee04819148e1adcfc874c2d0e6684d
Aishu Ammu <asm@asm-Vostro-260s>
2017-02-21 15:47:57 +0530

    1st

---
+++ add.py
@@ -0,0 +1,13 @@
+num1 = raw_input("enter the 1st number:")
+num1 = int(num1)
+num2 = raw_input("enter the 2nd number:")
+num2 = int(num2)
+opr = raw_input("enter any operator:")
+if opr == '+':
+	add = num1 + num2
+	print add
+elif oper == '-':
+	sub = num1 - num2
+	print sub 
+else:
+	print 'invalid'
