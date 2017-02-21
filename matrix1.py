a2eb57c9a859db663523e5ada0ca9a48865ff4ee
Aishu Ammu <asm@asm-Vostro-260s>
2017-02-21 16:07:18 +0530

    25th

---
+++ matrix1.py
@@ -0,0 +1,14 @@
+class matrix:
+	def __init__(self,array):
+		self.array=array
+	def __add__(self,other):
+		if len(self.array) = len(other.array):
+			row_len1 = self.array[0]
+			row_len2 = self.array[1]
+			for row in self.array:
+				if row_len1 = len(row):
+					return false
+			for row in other.array:
+				if row_len2 = len(row):
+					return false
+
