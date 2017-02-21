65a5f1642ccb2ecd2c8aa8f31322f8dc81068dc7
Aishu Ammu <asm@asm-Vostro-260s>
2017-02-21 15:51:38 +0530

    4th

---
+++ ex.py
@@ -0,0 +1,15 @@
+ch=raw_input("enter char")
+a=10
+b=5
+if ch== '+':
+   out=a + b
+   print out
+elif ch=='-':
+     out=a - b
+     print out
+elif ch=='*':
+     out=a * b
+     print out
+else:
+     out=a / b
+     print out
